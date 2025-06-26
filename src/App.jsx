a1ff10e41430eb2c9141b796d6d3a502031ffa38
João Pedro Moura Mentai <joao_pedro134@hotmail.com>
2025-06-26 15:39:00 +0100

    Add routing for authentication and homepage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-
 import './style/Login.css'
 import '../public/icons/css/icons.css';
+import {Route, Routes} from "react-router-dom";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Homepage from "./pages/Homepage.jsx";
 
 function App() {
 
   return (
     <>
-
+            <Routes>
+                <Route path="/" element={<Homepage/>}/>
+                <Route path="/registar" element={<SignUp/>}/>
+                <Route path="/entrar" element={<SignIn/>}/>
+            </Routes>
     </>
   )
 }
