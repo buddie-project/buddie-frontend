5a528d6c7bc81b27ea1f776e91246a55f0ccdc89
João Pedro Moura Mentai <joao_pedro134@hotmail.com>
2025-06-27 16:16:05 +0100

    Update navigation styles and add background image to homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,6 +2,11 @@ function Homepage() {
 
     return(
         <>
+            <div className="background-homepage">
+                <div className="background-image">
+                    <img src="/images/background-image.jpg" alt="Background Image" className="background-image"/>
+                </div>
+            </div>
         </>
     )
 }
