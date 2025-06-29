58b5abd7716056422ca38b5fc4712d17878c2f37
João Pedro Moura Mentai <joao_pedro134@hotmail.com>
2025-06-29 13:42:13 +0100

    Add interactive map to Homepage; implement new styles and animations for subject areas

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,6 +4,17 @@ function Homepage() {
     return(
         <>
             <div className="background-homepage" id="homepage"></div>
+            <div className="map">
+                <span className="area arts">artes</span>
+                <span className="area english">inglês</span>
+                <span className="area portuguese">português</span>
+                <span className="area geometry">geometria</span>
+                <span className="area design">design</span>
+                <span className="area architecture">arquitetura</span>
+                <span className="area math">matemática</span>
+                <span className="area philosophy">filosofia</span>
+            </div>
+
         </>
     )
 }
