4fdef6cb2568ec0d72f9e447b73b79c95c506ff4
Daniela Pinto <daniela.pinto.123@hotmail.com>
2025-07-03 23:10:10 +0100

    Enhance Navbar with `NavLink` for active link styling; update hover effects and improve link text consistency in CSS.

--- src/components/generalComponents/Navbar.jsx
+++ src/components/generalComponents/Navbar.jsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import {useState} from "react";
 
 function Navbar() {
@@ -26,11 +26,11 @@ function Navbar() {
             </h2>
 
             <div className={`nav-links ${menuOpen ? "open" : ""} ${searchOpen ? "search-open" : ""}`}>
-                <Link to="/" className="nav-link" onClick={() => setMenuOpen(false)}>homepage</Link>
-                <Link to="/sobre" className="nav-link" onClick={() => setMenuOpen(false)}>sobre</Link>
-                <Link to="/cursos" className="nav-link" onClick={() => setMenuOpen(false)}>cursos</Link>
-                <Link to="/entrar" className="nav-link" onClick={() => setMenuOpen(false)}>iniciar sessão</Link>
-                <Link to="/faqs" className="nav-link" onClick={() => setMenuOpen(false)}>faqs</Link>
+                <Link to="/" className="nav-link" onClick={() => setMenuOpen(false)}>página inicial</Link>
+                <NavLink to="/sobre" className="nav-link" onClick={() => setMenuOpen(false)}>sobre</NavLink>
+                <NavLink to="/cursos" className="nav-link" onClick={() => setMenuOpen(false)}>cursos</NavLink>
+                <NavLink to="/entrar" className="nav-link" onClick={() => setMenuOpen(false)}>iniciar sessão</NavLink>
+                <NavLink to="/faqs" className="nav-link" onClick={() => setMenuOpen(false)}>faqs</NavLink>
                 <i className="icon-search" onClick={handleSearchToggle} aria-hidden="true"></i>
                 { searchOpen && <input type="text" className="search-input" placeholder="Pesquisar..." />}
             </div>
