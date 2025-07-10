d575d4422237cc6c3b077397ace4018c65091383
João Pedro Moura Mentai <joao_pedro134@hotmail.com>
2025-07-10 18:06:19 +0100

    Refactor Courses component: add bookmark icon functionality, improve styling, and enhance user interaction

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -86,165 +86,166 @@ function Courses() {
         fetchFilterOptions();
     }, []);
 
-        const handleBookmark = (course) => {
-            setBookmarkedCourses((prev) => {
-                const isBookmarked = prev.some((c) => c.courseId === course.courseId);
-                return isBookmarked
-                    ? prev.filter((c) => c.courseId !== course.courseId)
-                    : [...prev, course];
-            });
-        };
+    const handleBookmark = (course) => {
+        setBookmarkedCourses((prev) => {
+            const isBookmarked = prev.some((c) => c.courseId === course.courseId);
+            return isBookmarked
+                ? prev.filter((c) => c.courseId !== course.courseId)
+                : [...prev, course];
+        });
+    };
 
-        useEffect(() => {
-            const stored = localStorage.getItem("bookmarkedCourses");
-            if (stored) {
-                setBookmarkedCourses(JSON.parse(stored));
-            }
-        }, []);
+    useEffect(() => {
+        const stored = localStorage.getItem("bookmarkedCourses");
+        if (stored) {
+            setBookmarkedCourses(JSON.parse(stored));
+        }
+    }, []);
 
-        useEffect(() => {
-            localStorage.setItem("bookmarkedCourses", JSON.stringify(bookmarkedCourses));
-        }, [bookmarkedCourses]);
+    useEffect(() => {
+        localStorage.setItem("bookmarkedCourses", JSON.stringify(bookmarkedCourses));
+    }, [bookmarkedCourses]);
 
-        const colors = ["red", "blue", "green", "orange", "purple", "yellow"];
+    const colors = ["red", "blue", "green", "orange", "purple", "yellow"];
 
-        const assignColors = (courses) => {
-            let lastColor = "";
-            return courses.map((course) => {
-                let availableColors = colors.filter(c => c !== lastColor);
-                let color = availableColors[Math.floor(Math.random() * availableColors.length)];
-                lastColor = color;
-                return {...course, color};
-            });
-        };
+    const assignColors = (courses) => {
+        let lastColor = "";
+        return courses.map((course) => {
+            let availableColors = colors.filter(c => c !== lastColor);
+            let color = availableColors[Math.floor(Math.random() * availableColors.length)];
+            lastColor = color;
+            return {...course, color};
+        });
+    };
 
-        useEffect(() => {
-            setCurrentPage(1);
-        }, [filters]);
+    useEffect(() => {
+        setCurrentPage(1);
+    }, [filters]);
 
-        const handleFilterChange = (filterName, value) => {
-            setFilters(prevFilters => ({
-                ...prevFilters,
-                [filterName]: value
-            }));
-        };
+    const handleFilterChange = (filterName, value) => {
+        setFilters(prevFilters => ({
+            ...prevFilters,
+            [filterName]: value
+        }));
+    };
 
-        return (
-            <>
-                <div className="filters">
-                    <div className="filters-box">
-                        <select className="option" onChange={(e) => handleFilterChange("curso", e.target.value)} value={filters.curso}>
-                            <option value="">curso</option>
-                            {courseNames.map(name => (
-                                <option key={name} value={name}>{name}</option>
-                            ))}
-                        </select>
-                        <select className="option" onChange={(e) => handleFilterChange("instituicao", e.target.value)} value={filters.instituicao}>
-                            <option value=" ">instituição</option>
-                            {institutionNames.map(name => (
-                                <option key={name} value={name}>{name}</option>
-                            ))}
-                        </select>
-                        <select className="option" onChange={e => handleFilterChange("area", e.target.value)} value={filters.area}>
-                                <option value="">area</option>
-                            {areas.map(area => (
-                                <option key={area} value={area}>{area}</option>
-                            ))}
-                        </select>
-                        <select className="option" onChange={e => handleFilterChange("distrito", e.target.value)} value={filters.distrito}>
-                                <option value=" ">distrito</option>
-                            {districts.map(district => (
-                                <option key={district} value={district}>{district}</option>
-                            ))}
-                        </select>
-                        <select className="option" onChange={(e) => handleFilterChange("estado", e.target.value)} value={filters.estado}>
-                            <option value=" ">estado</option>
-                            {status.map(status => (
-                                <option key={status} value={status}>{status}</option>
-                            ))}
-                        </select>
-                    </div>
+    return (
+        <>
+            <div className="filters">
+                <div className="filters-box">
+                    <select className="option" onChange={(e) => handleFilterChange("curso", e.target.value)} value={filters.curso}>
+                        <option value="">curso</option>
+                        {courseNames.map(name => (
+                            <option key={name} value={name}>{name}</option>
+                        ))}
+                    </select>
+                    <select className="option" onChange={(e) => handleFilterChange("instituicao", e.target.value)} value={filters.instituicao}>
+                        <option value=" ">instituição</option>
+                        {institutionNames.map(name => (
+                            <option key={name} value={name}>{name}</option>
+                        ))}
+                    </select>
+                    <select className="option" onChange={e => handleFilterChange("area", e.target.value)} value={filters.area}>
+                        <option value="">area</option>
+                        {areas.map(area => (
+                            <option key={area} value={area}>{area}</option>
+                        ))}
+                    </select>
+                    <select className="option" onChange={e => handleFilterChange("distrito", e.target.value)} value={filters.distrito}>
+                        <option value=" ">distrito</option>
+                        {districts.map(district => (
+                            <option key={district} value={district}>{district}</option>
+                        ))}
+                    </select>
+                    <select className="option" onChange={(e) => handleFilterChange("estado", e.target.value)} value={filters.estado}>
+                        <option value=" ">estado</option>
+                        {status.map(status => (
+                            <option key={status} value={status}>{status}</option>
+                        ))}
+                    </select>
                 </div>
+            </div>
 
-                <div className="applied-filters">
-                    <p>Filtros ativos:</p>
-                    {Object.entries(filters).map(
-                        ([key, value]) =>
-                            value && (
-                                <span key={key} className="filter-tag">
+            <div className="applied-filters">
+                <p>Filtros ativos:</p>
+                {Object.entries(filters).map(
+                    ([key, value]) =>
+                        value && (
+                            <span key={key} className="filter-tag">
                                 {key}: {value}
                             </span>
-                            )
-                    )}
+                        )
+                )}
+            </div>
+
+            <div className="courses-container">
+                {coursesData.map((course) => {
+                    return (
+                        <Link to={`/courses/${course.courseId}`} key={course.courseId}
+                              className={`course-card ${course.color}`}>
+                            <h3 className="course-header">
+                                <span className="course-name">{course.courseName}</span>
+                                <span
+                                    className={`icon-bookmark ${
+                                        bookmarkedCourses.some(
+                                            (c) => c.courseId === course.courseId
+                                        )
+                                            ? "active"
+                                            : ""
+                                    }`}
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        handleBookmark(course);
+                                    }}
+                                    aria-hidden="true"
+                                ></span>
+                            </h3>
+                            <section className="course-info">
+                                <h5>{course.fieldOfStudy}</h5>
+                                <p>
+                                    {course.institutionName ? (
+                                        <Link to={`/institutions/${course.institutionId}`}
+                                              style={{textDecorationColor: 'white'}}>
+                                            {course.institutionName}
+                                        </Link>
+                                    ) : (
+                                        "Desconhecida"
+                                    )}
+                                </p>
+                            </section>
+                        </Link>
+                    );
+                })}
+            </div>
+
+            <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={setCurrentPage}
+            />
+
+            <footer className="footer-courses">
+                <div className="footer-courses-content">
+                    <a
+                        href="https://github.com/buddie-project"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="github-link"
+                    >
+                        <img
+                            src="/images/github-mark.svg"
+                            className="github-icon"
+                            width="32"
+                            height="auto"
+                            alt="Github Logo"
+                        />
+                    </a>
+                    <p> | &copy; {new Date().getFullYear()} Buddie. All rights reserved.</p>
                 </div>
-
-                <div className="courses-container">
-                    {coursesData.map((course) => {
-                        return (
-                            <Link to={`/courses/${course.courseId}`} key={course.courseId}
-                                  className={`course-card ${course.color}`}>
-                                <h3 className="course-header">
-                                    <span className="course-name">{course.courseName}</span>
-                                    <span
-                                        className={`icon-bookmark ${
-                                            bookmarkedCourses.some(
-                                                (c) => c.courseId === course.courseId
-                                            )
-                                                ? "active"
-                                                : ""
-                                        }`}
-                                        onClick={(e) => {
-                                            e.preventDefault();
-                                            handleBookmark(course);
-                                        }}
-                                        aria-hidden="true"
-                                    ></span>
-                                </h3>
-                                <section className="course-info">
-                                    <h5>{course.fieldOfStudy}</h5>
-                                    <p>
-                                        {course.institutionName ? (
-                                            <Link to={`/institutions/${course.institutionId}`}
-                                                  style={{textDecorationColor: 'white'}}>
-                                                {course.institutionName}
-                                            </Link>
-                                        ) : (
-                                            "Desconhecida"
-                                        )}
-                                    </p>
-                                </section>
-                            </Link>
-                        );
-                    })}
-                </div>
-
-                <Pagination
-                    currentPage={currentPage}
-                    totalPages={totalPages}
-                    onPageChange={setCurrentPage}
-                />
-
-                <footer className="footer-courses">
-                    <div className="footer-courses-content">
-                        <a
-                            href="https://github.com/buddie-project"
-                            target="_blank"
-                            rel="noreferrer"
-                            className="github-link"
-                        >
-                            <img
-                                src="/images/github-mark.svg"
-                                className="github-icon"
-                                width="32"
-                                height="auto"
-                                alt="Github Logo"
-                            />
-                        </a>
-                        <p> | &copy; {new Date().getFullYear()} Buddie. All rights reserved.</p>
-                    </div>
-                </footer>
-            </>
-        );
+            </footer>
+        </>
+    );
 }
 
 export default Courses;
+
